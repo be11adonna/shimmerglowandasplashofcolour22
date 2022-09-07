17da4289194dfae54ce447a0ddd3d537efe4f2a7
be11adonna <113016882+be11adonna@users.noreply.github.com>
2022-09-07 21:36:03 +1000

    Adding column by column search

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function myFunction(column) {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
@@ -8,7 +8,7 @@ function myFunction() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[column];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
