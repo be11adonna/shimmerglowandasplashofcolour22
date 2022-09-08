35c8f731c1cabb3c9729c9bb92ac50b3dbfe7dd8
be11adonna <113016882+be11adonna@users.noreply.github.com>
2022-09-08 19:51:00 +1000

    Adding AJAX

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// import {parse} from './Recipes/node-csv/index.js';
-// parse(records, options, (err, data) => {
-//   console.info(data)
-// });
+import {parse} from './Recipes/node-csv/index.js';
+const httpRequest = new XMLHttpRequest();
+httpRequest.onreadystatechange = () => {
+  if (httpRequest.ReadyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      console.info(httpRequest.responseText);
+    }
+  }
+  // parse(records, options, (err, data) => {
+  //   console.info(data)
+  // });
+}
+httpRequest.open ('GET', 'https://be11adonna.github.io/shimmerglowandasplashofcolour22/Recipes-sparkle-22.csv', true);
+httpRequest.send();
+
 function myFunction(column) {
     // Declare variables
     var input, filter, table, tr, td, i, txtValue;
